259f5913b51b80eabdb2e22ef68152956f58447e
LimJaeHyeon9298 <limjh9298@gmail.com>
2025-02-20 11:50:41 +0900

    [fix] AddView View 분리 및 UI 수정 작업 진행중

--- Fitculator/Sources/Utils/Extensions/View+Extension.swift
+++ Fitculator/Sources/Utils/Extensions/View+Extension.swift
@@ -15,3 +15,31 @@ struct BackgroundView<Content: View>: View {
         }
     }
 }
+
+extension View {
+    func placeholder<Content: View>(
+        when shouldShow: Bool,
+        alignment: Alignment = .leading,
+        @ViewBuilder placeholder: () -> Content
+    ) -> some View {
+        ZStack(alignment: alignment) {
+            placeholder().opacity(shouldShow ? 1 : 0)
+            self
+        }
+    }}
+
+extension View {
+    func cornerRadius(_ radius: CGFloat, corners: UIRectCorner) -> some View {
+        clipShape(RoundedCorner(radius: radius, corners: corners))
+    }
+}
+
+struct RoundedCorner: Shape {
+    var radius: CGFloat = .infinity
+    var corners: UIRectCorner = .allCorners
+
+    func path(in rect: CGRect) -> Path {
+        let path = UIBezierPath(roundedRect: rect, byRoundingCorners: corners, cornerRadii: CGSize(width: radius, height: radius))
+        return Path(path.cgPath)
+    }
+}
