52f990e19612cfc6325ba913fa51adfddbbec4bb
MadCow <lobasketve@naver.com>
2025-02-27 18:22:08 +0900

    Error. HomeView 화면 이동 오류 수정

--- Fitculator/Sources/Presentation/Views/Home/HomeView.swift
+++ Fitculator/Sources/Presentation/Views/Home/HomeView.swift
@@ -30,10 +30,11 @@ struct HomeView: View {
             
             // TODO: - "<"누르면 저번주, 한번더 누를시 기간으로 나오게 + 네비게이션 바 추가
             // TODO: - isDisplayHome사용해 하위뷰 분기, CustomNavBar isDisplayHome에 넘겨주기
-            ZStack(alignment: .top) {
-                ScrollView(.vertical, showsIndicators: false) {
-                    VStack(spacing: 8) {
-                        CustomNavigationBar(
+            NavigationStack {            
+                ZStack(alignment: .top) {
+                    ScrollView(.vertical, showsIndicators: false) {
+                        VStack(spacing: 8) {
+                            CustomNavigationBar(
                             isDisplayHome: true,
                             homeBtnAction: {
                             
@@ -42,55 +43,56 @@ struct HomeView: View {
                                 
                                 viewModel.fetchDataForDateHistory(date)
                             }, notificationBtnAction: {})
-                            .zIndex(1)
-                            .frame(height: 44)
-                        
-                        WorkoutDonutChart(
-                            originalTotal: viewModel.originalTotal,
-                            totalPct: viewModel.totalPct,
-                            remainingPct: viewModel.remainingPct,
-                            changedTraningRecordsData: viewModel.changedTraningRecordsData,
-                            traningRecords: viewModel.traningRecords,
-                            activeChartData: viewModel.activeChartData
-                        )
+                                .zIndex(1)
+                                .frame(height: 44)
+                            
+                            WorkoutDonutChart(
+                                originalTotal: viewModel.originalTotal,
+                                totalPct: viewModel.totalPct,
+                                remainingPct: viewModel.remainingPct,
+                                changedTraningRecordsData: viewModel.changedTraningRecordsData,
+                                traningRecords: viewModel.traningRecords,
+                                activeChartData: viewModel.activeChartData
+                            )
                             .frame(height: viewHeight * 0.4)
                             .padding(.horizontal, horizontalPadding)
                             .padding(.vertical, verticalPadding)
-                        
-                        FatigueChart(
-                            fatigueValue: viewModel.fatigueValue,
-                            changedTraningRecordsData: viewModel.changedTraningRecordsData,
-                            traningRecords: viewModel.traningRecords
-                        )
+                            
+                            FatigueChart(
+                                fatigueValue: viewModel.fatigueValue,
+                                changedTraningRecordsData: viewModel.changedTraningRecordsData,
+                                traningRecords: viewModel.traningRecords
+                            )
                             .frame(height: viewHeight * 0.13)
                             .padding(.top, 16)
                             .padding(.horizontal, horizontalPadding)
                             .padding(.vertical, verticalPadding)
-                        
-                        WeeklyStrengthReps(
-                            changedTraningRecordsData: viewModel.changedTraningRecordsData,
-                            traningRecords: viewModel.traningRecords, workoutList: viewModel.workoutList
-                        )
+                            
+                            WeeklyStrengthReps(
+                                changedTraningRecordsData: viewModel.changedTraningRecordsData,
+                                traningRecords: viewModel.traningRecords, workoutList: viewModel.workoutList
+                            )
                             .frame(height: viewHeight * 0.1)
                             .padding(.horizontal, horizontalPadding)
                             .padding(.vertical, verticalPadding)
-                        
-                        WorkoutHistory(traningRecords: viewModel.traningRecords)
-                            .padding(.horizontal, horizontalPadding)
-                            .padding(.vertical, verticalPadding)
+                            
+                            WorkoutHistory(traningRecords: viewModel.traningRecords)
+                                .padding(.horizontal, horizontalPadding)
+                                .padding(.vertical, verticalPadding)
+                        }
                     }
+                    .refreshable {
+                        viewModel.fetchWorkoutHistory()
+                        viewModel.fetchWorkoutLists()
+                    }
+                    .padding(.horizontal, horizontalPadding)
+                    .padding(.vertical, verticalPadding)
                 }
-                .refreshable {
+                .background(Color.fitculatorBackgroundColor)
+                .onAppear {
                     viewModel.fetchWorkoutHistory()
-                    viewModel.fetchWorkoutLists()
+                    viewModel.updateDonutChartData()
                 }
-                .padding(.horizontal, horizontalPadding)
-                .padding(.vertical, verticalPadding)
-            }
-            .background(Color.fitculatorBackgroundColor)
-            .onAppear {
-                viewModel.fetchWorkoutHistory()
-                viewModel.updateDonutChartData()
             }
             .onChange(of: selectedDate) { newValue, oldValue in
                 print("선택된 날짜 변경됨: \(newValue)")
